61cef69d130758ce2fcbb0a7a57bbccbdf8c32ff
PawanNayanajith <djnayanajith@gmail.com>
2018-11-24 12:23:11 +0530

    automation-test

--- automation/src/test/java/demo/LoginTest.java
+++ automation/src/test/java/demo/LoginTest.java
@@ -27,7 +27,7 @@ public class LoginTest extends BaseTest {
 
 	@AfterClass(alwaysRun = true)
 	public void tearDown() throws Exception {
-		// driver.quit();
+		 driver.quit();
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
